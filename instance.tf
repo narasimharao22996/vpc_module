092e7f3dfa6731882cabb6b6b8dc5395b5f3c3d6
narasimharao <110323998+narasimharao22996@users.noreply.github.com>
2023-01-04 19:04:51 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,13 @@
+terraform {
+backend "s3" {
+  bucket = "mys3baccc"
+  key = "folder2"
+  region = "us-east-1"
+  dynamodb_table = "dynamodbtableee"
+ 
+}
+}
+
 resource "aws_key_pair" "key" {
   key_name   = "key_1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlYfNJlNbujyMblRhg0Zwl9ZjcnUu1ZvkDkXBDVBXTsc14sPTqxutYxUKdv+euqxJd43KfMDcXrzVj8FN6UJb6ye+FNd/sh4n7eAUwtkYG6zptDWlGv/H6TKnTuiTjHfdYn4nwy49t5vTsmHTgevoAl37mr3yIOEwejTyqHxXKTLy9tYOeHaNzZNkcsiGDB94ELAEn8pBfa4a73TBmK+YqRuPVf+A8kyZfGIu62zSMgjSVgErrmV1oqpKI0oU5c0HWnN99YgxAetQYG/FXuJwOU/X0kWeaF7Aw+j46px+pm3ZoXwYzuBCezdoPpa30mmIaLruHvkrfK6SiT/gmAKi+wbXAQhEbHV5ipjcLkxbfV59YGveMcPnvooZFEaNFBQUj5knGVwORtRgPc5ZMAUUdLhv09tJ7V7XIMGU0fz1/1T3uW2uug4ReSk4kP84mI7+pxAsVwZeTUZb4ob9o8SAOTc1T7kpNGGDYv8R/uhXtLwSSs5SSq537HRqiQMbapVc= admin@DESKTOP-TC12A8K"
